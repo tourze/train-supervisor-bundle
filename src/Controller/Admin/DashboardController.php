bd145674eed9f2d015e5d229b936914a40cc118d
github-ci <you@example.com>
2025-06-11 00:02:30 +0800

    修正过时用法

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,7 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Tourze\TrainSupervisorBundle\Entity\ProblemTracking;
 use Tourze\TrainSupervisorBundle\Entity\QualityAssessment;
 use Tourze\TrainSupervisorBundle\Entity\SupervisionInspection;
